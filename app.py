1739bead08017278bb92e19a2fb68c8446d4837c
xplorerh <080bei010@ioepc.edu.np>
2025-01-25 06:56:41 +0545

    alikati worked on waste_management

--- app.py
+++ app.py
@@ -195,6 +195,5 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('home'))
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
